e70afb8c764953a13232fb13e9fe40c0ed758ae0
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-12-07 00:33:13 -0300

    Instalando react, react router dom

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,38 +1,19 @@
 import React from "react";
-
-// or less ideally
-import { Jumbotron, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}></Route>
+          <Route path="/signin" component={Signin}></Route>
+          <Route path="/signup" component={Signup}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
